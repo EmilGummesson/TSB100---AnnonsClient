97afdcf650844ee861a78fb52e760f85206523aa
Erik Jiremalm <erji0002@student.hv.se>
2019-05-17 11:15:31 +0200

    inmatning login sessen funka dock inte. fixa även asp länk istälent för lokal i heder

--- AnnonsTjanst/Controllers/HomeController.cs
+++ AnnonsTjanst/Controllers/HomeController.cs
@@ -8,17 +8,60 @@ namespace AnnonsTjanst.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+       
+        
+        public ActionResult Index(int? idin)
         {
+            if (idin == null)
+            {
+                try
+                {
+                    int id = 11;
+                    loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
+                    //id = int.Parse(Session["profilId"]);ut komenterad kod pga problem med sekson
+                    var anvendare = logclient.VisaAnvandarInfoId(id);
+                    ViewBag.medalande = anvendare.Anvandarnamn;
+                }
+                catch
+                {
+
+                }
+            }
+            else
+            {
+                int id = int.Parse(idin.ToString());
+                loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
+                if (logclient.VerifieraInloggning(id))
+                {
+                    Session["profilId"] = id;
+                    var anvendare = logclient.VisaAnvandarInfoId(id);
+                }
+            }
             ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
             return View(client.HamtaAllaAnnonser());
         }
 
-        public ActionResult About()
+        public ActionResult logain()
         {
-            ViewBag.Message = "Your application description page.";
+            loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
+            var test = logclient.VerifieraInloggning(1337);
 
             return View();
+        }
+        [HttpPost]
+        public ActionResult logain(string anvandarnamn, string losenord, string behorrighet)
+        {
+            if (behorrighet == null)
+            {
+                behorrighet = "standard";
+            }
+            loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
+            var anvinfo = logclient.LoggaIn(anvandarnamn, losenord, behorrighet);
+            if (anvinfo != null)
+            {
+                Session["profilId"] = anvinfo.Id.ToString();
+            }
+            return RedirectToAction("Index");
         }
 
         public ActionResult Contact()
@@ -28,6 +71,6 @@ namespace AnnonsTjanst.Controllers
             return View();
         }
 
-        
+
     }
 }
