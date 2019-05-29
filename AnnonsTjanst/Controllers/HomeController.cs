5a3bad933f3f45fa1b3bfbf60451c91922b4c076
Mask Järn <47307674+Jernkastel@users.noreply.github.com>
2019-05-29 15:32:47 +0200

    Added comments and fixed spelling errors etc.

--- AnnonsTjanst/Controllers/HomeController.cs
+++ AnnonsTjanst/Controllers/HomeController.cs
@@ -8,43 +8,40 @@ namespace AnnonsTjanst.Controllers
 {
     public class HomeController : Controller
     {
-
-
-
         public ActionResult Index(int? id)
         {
+            //Kollar sessionsID och sätter användarnamnet i en Viewbag
+            //Är användaren inte inloggad, dirigeras denne om till inloggningssidan
             if (id == null)
             {
                 try
                 {
-                    int nyid = 11;
+                    int tempId = 11;
                     loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
                     if (Session["profilId"] != null)
                     {
                         //Converting your session variable value to integer
 
-                        nyid = Convert.ToInt32(Session["profilId"]);//ut komenterad kod pga problem med sekson   Convert.ToInt32(id1.Text); ((int)Session["profilId"]);
-                        var anvendare = logclient.VisaAnvandarInfoId(nyid);
-                        ViewBag.medalande = anvendare.Anvandarnamn;
+                        tempId = Convert.ToInt32(Session["profilId"]);
+                        //Koden är utkommenterad på grund av problem med sessionen
+                        //Convert.ToInt32(id1.Text); ((int)Session["profilId"]);
+                        var anvandare = logclient.VisaAnvandarInfoId(tempId);
+                        ViewBag.medalande = anvandare.Anvandarnamn;
                     }
-
-
                 }
                 catch
                 {
-
                 }
             }
             else
             {
-                int idny = int.Parse(id.ToString());
+                int tempId = int.Parse(id.ToString());
                 loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
-                if (logclient.VerifieraInloggning(idny))
+                if (logclient.VerifieraInloggning(tempId))
                 {
-                    Session["profilId"] = idny;
-                    var anvendare = logclient.VisaAnvandarInfoId(idny);
-                    ViewBag.medalande = anvendare.Anvandarnamn;
-
+                    Session["profilId"] = tempId;
+                    var anvandare = logclient.VisaAnvandarInfoId(tempId);
+                    ViewBag.medalande = anvandare.Anvandarnamn;
                 }
                 else
                 {
@@ -53,44 +50,48 @@ namespace AnnonsTjanst.Controllers
                 }
             }
             ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
-            ViewBag.id =Convert.ToInt32(Session["profilId"]);
+            //Skickar med sessionsID i en Viewbag
+            ViewBag.id = Convert.ToInt32(Session["profilId"]);
+            //Returnerar en lisa på alla aktuella annonser
             return View(client.HamtaAllaAnnonser());
         }
 
-        public ActionResult logain()
+        public ActionResult Login()
         {
             loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
             var test = logclient.VerifieraInloggning(1337);
 
             return View();
         }
+
         [HttpPost]
-        public ActionResult logain(string anvandarnamn, string losenord, string behorrighet)
+        public ActionResult Login(string anvandarnamn, string losenord, string behorighet)
         {
-            if (behorrighet == null)
+            //Om inget annat angivs, sätts användarens behörighet till standardbehörighet
+            if (behorighet == null)
             {
-                behorrighet = "standard";
+                behorighet = "standard";
             }
             loginReferences.InloggningServiceClient logclient = new loginReferences.InloggningServiceClient();
-            var test2 = logclient.HamtaAllaAnvandare();
-            var anvinfo = logclient.LoggaIn(anvandarnamn, losenord, behorrighet);
+            var test = logclient.HamtaAllaAnvandare();
+            //Loggar in med de angivna användaruppgifterna + behörighet
+            var anvinfo = logclient.LoggaIn(anvandarnamn, losenord, behorighet);
             if (anvinfo != null)
             {
 
-                Session["profilId"] = anvinfo.ProfilId.ToString(); //) ["profilId"] = Convert.ToInt32(anvinfo.ProfilId.ToString());
+                Session["profilId"] = anvinfo.ProfilId.ToString(); 
+                //["profilId"] = Convert.ToInt32(anvinfo.ProfilId.ToString());
                 Session["inlogad"] = "true";
             }
             return RedirectToAction("Index");
         }
 
+        //Denna vy används ej
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
-        
-
-
     }
 }
